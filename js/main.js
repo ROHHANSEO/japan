f0253662b4f03289afde02a06d3e51e8c54e2758
Roh <sola4441@gmail.com>
2022-11-13 23:44:29 +0900

    asd

--- js/main.js
+++ js/main.js
@@ -1,35 +1,38 @@
 $("#menu_btn").click(function(){
     $("#title").text("교토 여행");
 
-    $("#main_nav").show();
-    $(".sca").hide();
-    $(".first_list").show();
-    $(this).hide();
-    $("#menu_back_btn").show();
+
+
+    $("#main_nav").addClass("on");
     $(".first_list").addClass("on");
+    $("#menu_btn").hide();
+    $("#menu_back_btn").show();
     $("#all").show();
+    $("#menu_back_btn").attr('data-page','1');
 })
 
 $(".first_list > li:not(:eq(0))").click(function(){
     var target = $(this).attr("data-target");
 
-    $(this).parent().hide();
     $("#menu_back_btn").show();
+    $(".first_list").removeClass("on");
     $("#"+target).addClass("on");
-    $("#"+target).show();
+    $("#menu_back_btn").attr('data-page','2');
 })
 
 $("#menu_back_btn").click(function(){
-    if(!$(".first_list").hasClass("on")){
-        $(".on").hide();
-        $(".first_list").show();
-        $("#menu_back_btn").hide();
-    } else {
-        $("#main_nav").hide();
-        $(".first_list").hide();
+
+    var page = $(this).attr("data-page");
+
+    if(page == '1'){
+        $("#main_nav").removeClass("on");
+        $("#all").hide();
         $("#menu_back_btn").hide();
         $("#menu_btn").show();
-        $("#all").hide();
+    } else if(page == '2') {
+        $('.sca').removeClass("on");
+        $(".first_list").addClass("on");
+        $("#menu_back_btn").attr('data-page','1');
     }
 })
 
@@ -37,13 +40,20 @@ $(".sca .day_list li:not(:eq(0))").click(function(){
     var target = $(this).attr("day-calendar");
     var targetTitle = $(this).parent().parent().parent().attr("id");
 
-    $("#main_section div").hide();
+    
+    $("#main_section div").removeClass("on");
+    $('.sca').removeClass("on");
     $("#menu_back_btn").hide();
-    $("#main_nav").hide();
     $("#all").hide();
     $("#title").text(targetTitle);
     $("#menu_btn").show();
     $("#"+target).addClass("on");
-    $("#"+target).show();
+    $("#main_nav").removeClass("on");
+})
+
+$(".siteBtn").click(function(){
+    var href = $(this).prev().attr("href");
+
+    location.href = href;
 })
 
